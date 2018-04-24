c73873b01f1ebd2431583fef5374bd04e7c1e351
Mr PoTaTo <insertoriginalemailhereplease@gmail.com>
2018-04-24 20:54:34 +1000

    Setup basic scene and programmed basic movement

---
+++ Labyrinth Lucy/Assets/Scripts/playerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float Speed = 7.0f;
+
+    public LayerMask groundLayers;
+
+    public float jumpForce = 4.0f;
+
+    public CapsuleCollider col;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+    }
+
+    void FixedUpdate()
+    {
+        float translation = Input.GetAxis("Vertical") * Speed;
+        float strafe = Input.GetAxis("Horizontal") * Speed;
+        translation *= Time.deltaTime;
+        strafe *= Time.deltaTime;
+
+        transform.Translate(strafe, 0, translation);
+
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+    }
+
+}
