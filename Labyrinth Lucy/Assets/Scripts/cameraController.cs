be2862f85304e07fe781a1658c0d199af9137cb8
Mr PoTaTo <insertoriginalemailhereplease@gmail.com>
2018-05-29 19:47:38 +1000

    Added basic animations, added a base model, added 3rd person camera movement with mouse.

---
+++ Labyrinth Lucy/Assets/Scripts/cameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+	public float Sensitivity = 10.0f;
+	public Transform Player;
+	public float Offset = 2;
+	public Vector2 pitchMinMax = new Vector2(-40, 85);
+
+	public float rotationSmoothTime = 0.12f;
+	Vector3 rotationSmoothVelocity;
+	Vector3 currentRoatation;
+
+	float yaw;
+	float pitch;
+
+	void LateUpdate()
+	{
+		yaw += Input.GetAxis("Mouse X") * Sensitivity;
+		pitch -= Input.GetAxis("Mouse Y") * Sensitivity;
+		pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+		currentRoatation = Vector3.SmoothDamp(currentRoatation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+		transform.eulerAngles = currentRoatation;
+
+		transform.position = Player.position - transform.forward * Offset;
+	}
+
+}
--- Labyrinth Lucy/Assets/Scripts/playerController.cs
+++ Labyrinth Lucy/Assets/Scripts/playerController.cs
@@ -2,44 +2,80 @@
 
 public class playerController : MonoBehaviour {
 
-    private Rigidbody rb;
+	private Rigidbody rb;
 
-    public float Speed = 7.0f;
-
-    public LayerMask groundLayers;
-
+    public float walkSpeed = 2.0f;
+	public float runSpeed = 6.0f;
     public float jumpForce = 4.0f;
+	public float turnSmoothTime = 0.2f;
+	float turnSmoothVelocity;
+	public float speedSmoothTime = 0.1f;
+	float speedSmoothVelocity;
+	float currentSpeed;
+
+	bool spaceKey;
 
     public CapsuleCollider col;
+	public LayerMask groundLayers;
+
+	Animator animator;
+	public Transform cameraT;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         rb = GetComponent<Rigidbody>();
         col = GetComponent<CapsuleCollider>();
+		animator = GetComponent<Animator>();
+		spaceKey = false;
+		
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        float translation = Input.GetAxis("Vertical") * Speed;
-        float strafe = Input.GetAxis("Horizontal") * Speed;
-        translation *= Time.deltaTime;
-        strafe *= Time.deltaTime;
+		Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		Vector2 inputDir = input.normalized;
 
-        transform.Translate(strafe, 0, translation);
+		if (inputDir != Vector2.zero)
+		{
+			float targetRot = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+			transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRot, ref turnSmoothVelocity, turnSmoothTime);
+		}
 
-        if (Input.GetKeyDown("escape"))
+		bool running = Input.GetKey(KeyCode.LeftShift);
+		float targetSpeed = ((running) ? runSpeed : walkSpeed * inputDir.magnitude);
+		currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+		transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+
+		float animationSpeedPercent = ((running) ? 1 : .5f) * inputDir.magnitude;
+		animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+
+		if (Input.GetKeyDown("escape"))
         {
             Cursor.lockState = CursorLockMode.None;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			spaceKey = true;
+		}
+		else
+		{
+			spaceKey = false;
+		}
+
     }
 
-    private bool isGrounded()
+	void FixedUpdate()
+	{
+		if (spaceKey && isGrounded())
+		{
+			rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+		}
+	}
+
+	private bool isGrounded()
     {
         return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
     }
